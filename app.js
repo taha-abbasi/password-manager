ec2afe7dcb66f013d8baef9a23156afb7a867f10
Taha Abbasi <tahaabbasi@me.com>
2017-11-13 00:15:18 -0500

    Account management demo

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log('starting password manager');
+
+var storage = require('node-persist');
+storage.initSync();
+
+// storage.setItemSync('accounts', [{
+//     username: 'Andrew',
+//     balance: 0
+// }]);
+
+var accounts = storage.getItemSync('accounts');
+
+console.log(accounts);
+
+// accounts.push({
+//     username: 'James',
+//     balance: 100
+// });
+//
+// storage.setItemSync('accounts', accounts);
+
