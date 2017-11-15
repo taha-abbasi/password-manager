d1f563ad804a8f5c5957763ac7d7fc999a7fab31
Taha Abbasi <tahaabbasi@me.com>
2017-11-15 15:17:52 -0500

    completed args demo user input

--- example-args.js
+++ example-args.js
@@ -1,4 +1,20 @@
-var argv = require('yargs').argv;
+var argv = require('yargs')
+    .command('hello', 'Greets the user', function (yargs) {
+        yargs.options({
+            name: {
+                demand: true,
+                alias: 'n',
+                description: 'Your first name goes here'
+            },
+            lastname: {
+                demand: true,
+                alias: 'l',
+                description: 'Your last name goes here'
+            }
+        }).help('help');
+    })
+    .help('help')
+    .argv;
 
 var command = argv._[0];
 
